0bcef3e0980e366237a57a207c81dd3dc33af3b5
Rota1001 <johnny1001s000602@gmail.com>
2024-06-27 04:44:00 +0800

    time

--- kernel/info.h
+++
@@ -1,2 +0,0 @@
-#include "types.h"
-uint64 systeminfo(uint64 st);
--- kernel/sysinfo.c
+++ kernel/sysinfo.c
@@ -1,10 +1,18 @@
 #include "types.h"
-#include "info.c"
-// #include "sysinfo.h"
+#include "defs.h"
+#include "sysinfo.h"
 
 
 
-
+uint64 systeminfo(uint64 addr){
+    struct proc *p = myproc();
+    struct sysinfo info;
+    info.freemem = memorycount();
+    info.nproc = proc_count();
+    if(copyout(p->pagetable, addr, (char *)&info, sizeof(info)) < 0)
+        return -1;
+    return 0;
+}
 uint64 sys_sysinfo(void){
     uint64 st;
     argaddr(0, &st);
